2810cd13fb68830bf9f03a14ad0fdc7f150abc89
Takuya3627 <tm.0630.ppp@gmail.com>
2021-03-10 13:01:21 +0000

    Add Commti

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -11,16 +11,24 @@ class SearchsController < ApplicationController
 
   def search_for(model, content, method)
     if model == 'user'
-      if method == 'perfect'
+      if method == "perfect"
         User.where(name: content)
-      else
-        User.where('name LIKE ?', '%'+content+'%' )
+      elsif method == "forward"
+        User.where("name LIKE?", "#{content}%" )
+      elsif method == "backward"
+        User.where("name LIKE?", "%#{content}" )
+      elsif method == "partial"
+        User.where("name LIKE?", '%'+content+'%' )
       end
     elsif model == 'book'
       if method == 'perfect'
         Book.where(title: content)
-      else
-        Book.where('title LIKE ?', '%'+content+'%' )
+      elsif method == "forward"
+        Book.where("title LIKE?", "#{content}%" )
+      elsif method == "backward"
+        Book.where("title LIKE?", "%#{content}" )
+      elsif method == "partial"
+        Book.where("title LIKE?", '%'+content+'%' )
       end
     end
   end
